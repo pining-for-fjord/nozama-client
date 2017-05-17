e40a00bebc044c49b62afc16170b7dc3d1225f16
jscohen <jon.cohen08@gmail.com>
2017-05-17 15:23:27 -0400

    missed a file in previous commit

--- assets/scripts/carts.js
+++ assets/scripts/carts.js
@@ -1,81 +1,81 @@
 'use strict'
 
-/* Set rates + misc */
-let taxRate = 0.05;
-let shippingRate = 0;
-let fadeTime = 300;
+const cartExport = function () {
+  /* Set rates + misc */
+  let taxRate = 0.05
+  let shippingRate = 0
+  let fadeTime = 300
+
+  /* Assign actions */
+  $('.product-quantity input').change( function() {
+    updateQuantity(this)
+  })
+
+  $('.product-removal button').click( function() {
+    removeItem(this)
+  })
 
 
-/* Assign actions */
-$('.product-quantity input').change( function() {
-  updateQuantity(this);
-});
+  /* Recalculate cart */
+  function recalculateCart()
+  {
+    let subtotal = 0
 
-$('.product-removal button').click( function() {
-  removeItem(this);
-});
+    /* Sum up row totals */
+    $('.product').each(function () {
+      subtotal += parseFloat($(this).children('.product-line-price').text())
+    })
+
+    /* Calculate totals */
+    let tax = subtotal * taxRate
+    let shipping = (subtotal > 0 ? shippingRate : 0)
+    let total = subtotal + tax + shipping
+
+    /* Update totals display */
+    $('.totals-value').fadeOut(fadeTime, function() {
+      $('#cart-subtotal').html(subtotal.toFixed(2))
+      $('#cart-tax').html(tax.toFixed(2))
+      $('#cart-shipping').html(shipping.toFixed(2))
+      $('#cart-total').html(total.toFixed(2))
+      if(total == 0){
+        $('.checkout').fadeOut(fadeTime)
+      }else{
+        $('.checkout').fadeIn(fadeTime)
+      }
+      $('.totals-value').fadeIn(fadeTime)
+    })
+  }
 
 
-/* Recalculate cart */
-function recalculateCart()
-{
-  let subtotal = 0;
+  /* Update quantity */
+  function updateQuantity(quantityInput)
+  {
+    /* Calculate line price */
+    let productRow = $(quantityInput).parent().parent()
+    let price = parseFloat(productRow.children('.product-price').text())
+    let quantity = $(quantityInput).val()
+    let linePrice = price * quantity
 
-  /* Sum up row totals */
-  $('.product').each(function () {
-    subtotal += parseFloat($(this).children('.product-line-price').text());
-  });
+    /* Update line price display and recalc cart totals */
+    productRow.children('.product-line-price').each(function () {
+      $(this).fadeOut(fadeTime, function() {
+        $(this).text(linePrice.toFixed(2))
+        recalculateCart()
+        $(this).fadeIn(fadeTime)
+      })
+    })
+  }
 
-  /* Calculate totals */
-  let tax = subtotal * taxRate;
-  let shipping = (subtotal > 0 ? shippingRate : 0);
-  let total = subtotal + tax + shipping;
-
-  /* Update totals display */
-  $('.totals-value').fadeOut(fadeTime, function() {
-    $('#cart-subtotal').html(subtotal.toFixed(2));
-    $('#cart-tax').html(tax.toFixed(2));
-    $('#cart-shipping').html(shipping.toFixed(2));
-    $('#cart-total').html(total.toFixed(2));
-    if(total == 0){
-      $('.checkout').fadeOut(fadeTime);
-    }else{
-      $('.checkout').fadeIn(fadeTime);
-    }
-    $('.totals-value').fadeIn(fadeTime);
-  });
+  /* Remove item from cart */
+  function removeItem(removeButton)
+  {
+    /* Remove row from DOM and recalc cart total */
+    let productRow = $(removeButton).parent().parent()
+    productRow.slideUp(fadeTime, function() {
+      productRow.remove()
+      recalculateCart()
+    })
+  }
 }
 
-
-/* Update quantity */
-function updateQuantity(quantityInput)
-{
-  /* Calculate line price */
-  let productRow = $(quantityInput).parent().parent();
-  let price = parseFloat(productRow.children('.product-price').text());
-  let quantity = $(quantityInput).val();
-  let linePrice = price * quantity;
-
-  /* Update line price display and recalc cart totals */
-  productRow.children('.product-line-price').each(function () {
-    $(this).fadeOut(fadeTime, function() {
-      $(this).text(linePrice.toFixed(2));
-      recalculateCart();
-      $(this).fadeIn(fadeTime);
-    });
-  });
-}
-
-
-/* Remove item from cart */
-function removeItem(removeButton)
-{
-  /* Remove row from DOM and recalc cart total */
-  let productRow = $(removeButton).parent().parent();
-  productRow.slideUp(fadeTime, function() {
-    productRow.remove();
-    recalculateCart();
-  });
-}
-
-module.exports = true
+module.exports = cartExport
